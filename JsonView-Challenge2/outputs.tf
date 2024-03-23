0ac642721ec3e92f490bcb54264957a2e57b52ef
Rahul KHAVATAKOPPA <AL1086005@admin.hubtotal.net>
2024-03-23 14:53:09 +0530

    fix:

--- JsonView-Challenge2/outputs.tf
+++
@@ -1,6 +0,0 @@
-####################################################
-#  OUTPUT  the instance meta data of json file    #
-###################################################
-output "instance_metadata_json" {
-  value = jsondecode(data.local_file.instance_metadata_file.content)
-}
